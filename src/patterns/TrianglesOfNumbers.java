2772ee14eb727e681eb73c9c2d0f6dd87f3e6b2a
Amit Upadhyay <amitu2016@gmail.com>
2022-11-27 19:54:28 +0530

    Merge branch 'main' of https://github.com/amitu2016/coding-ninjas-dsa-questions.git

--- src/patterns/TrianglesOfNumbers.java
+++ src/patterns/TrianglesOfNumbers.java
@@ -1,10 +1,36 @@
 package patterns;
 
-public class TrianglesOfNumbers {
+import java.util.Scanner;
 
+public class TrianglesOfNumbers {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 1;
+		while (i <= n) {
+			int space = 1;
+			while (space <= n - i) {
+				System.out.print(" ");
+				space++;
+			}
+			int j = 1;
+			int p = i;
+			while (j <= i) {
+				System.out.print(p);
+				p++;
+				j++;
+			}
+			j = 1;
+			p = (i - 1) * 2;
+			while (j <= i - 1) {
+				System.out.print(p);
+				p--;
+				j++;
+			}
+			i++;
+			System.out.println();
+		}
 
 	}
-
 }
