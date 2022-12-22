4eefaf7ceaf62e2855fc02e2b8b03e88161ad957
Amit Upadhyay <amitu2016@gmail.com>
2022-12-22 08:09:18 +0530

    commit

--- src/hashmap/Map.java
+++ src/hashmap/Map.java
@@ -22,6 +22,51 @@ public class Map<K, V> {
 		return hashCode % numBuckets;
 	}
 	
+	public int size() {
+		return size;
+	}
+	
+	
+	public V getValue(K key) {
+		int bucketIndex = getBucketIndex(key);
+		MapNode<K, V> head = buckets.get(bucketIndex);
+		while (head != null) {
+			
+			if (head.key.equals(key)) {
+				return head.value;
+			}
+			
+			head = head.next;
+		}
+		return null;
+	}
+	
+	public V remove(K key) {
+		int bucketIndex = getBucketIndex(key);
+		MapNode<K, V> head = buckets.get(bucketIndex);
+		MapNode<K, V> prev = null;
+		while (head != null) {
+			
+			if (head.key.equals(key)) {
+				size--;
+				if (prev ==  null) {
+					buckets.set(bucketIndex, head.next);
+				} else {
+					prev.next = head.next;
+				}
+				
+				return head.value;
+			}
+			prev = head;
+			head = head.next;
+		}
+		return null;
+	}
+	
+	public double getLoadFactor() {
+		return (1.0*size) / numBuckets;
+	}
+	
 	public void insert(K key, V value) {
 		int bucketIndex = getBucketIndex(key);
 		MapNode<K, V> head = buckets.get(bucketIndex);
@@ -37,8 +82,40 @@ public class Map<K, V> {
 		
 		head = buckets.get(bucketIndex);
 		MapNode<K, V> newMapNode = new MapNode<K, V>(key, value);
+		size++;
 		newMapNode.next = head;
 		buckets.set(bucketIndex, newMapNode);
+		double loadFactor = (1.0*size) / numBuckets;
+		if(loadFactor > 0.7) {
+			rehash();
+		}
+	}
+
+	private void rehash() {
+		ArrayList<MapNode<K, V>> temp = buckets;
+		buckets = new ArrayList<>();
+		
+		for (int i = 0; i < 2*numBuckets; i++) {
+			buckets.add(null);
+		}
+		
+		size = 0;
+		numBuckets *= 2;
+		
+		for (int i = 0; i < temp.size(); i++) {
+			MapNode<K, V> head = temp.get(i);
+			
+			while (head != null) {
+				K key = head.key;
+				V value = head.value;
+				
+				insert(key, value);
+				head = head.next;
+			}
+			
+			
+		}
+		
 	}
 	
 }
